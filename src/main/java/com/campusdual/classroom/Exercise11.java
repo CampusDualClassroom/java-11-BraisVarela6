c1fffdb1eef79fa82477b81ff3258d1388a15b6b
BraisVarela <braisvarela6@gmail.com>
2024-10-16 13:52:36 +0200

    Ejercicio 11 Hecho

--- src/main/java/com/campusdual/classroom/Exercise11.java
+++ src/main/java/com/campusdual/classroom/Exercise11.java
@@ -6,6 +6,32 @@ public class Exercise11 {
         TVRemote blackRemote = new TVRemote("Negro");
         System.out.println(redRemote.getColor());
         System.out.println(blackRemote.getColor());
+
+
+
+
+        if (redRemote.isOn()){
+            redRemote.turnOff();
+            System.out.println("La TV se acaba de apagar");
+        } else {
+            redRemote.turnOn();
+            System.out.println("La TV se acaba de encender");
+        }
+
+        redRemote.channelUp();
+
+        do {
+            redRemote.channelDown();
+        } while (redRemote.getChannel() >= 0);
+
+        redRemote.volumeUp();
+
+        do {
+            redRemote.volumeDown();
+        } while (redRemote.getVolume() >= 0);
+
+
+        redRemote.turnOff();
     }
 
 }
